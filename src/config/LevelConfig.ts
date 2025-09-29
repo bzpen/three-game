8ffd047685c67046ace5e33f147807173299cd8f
bizhipeng <3118659412@qq.com>
2025-09-29 17:03:43 +0800

    feat: 新增关卡4 5 未完成

--- src/config/LevelConfig.ts
+++ src/config/LevelConfig.ts
@@ -1,3 +1,3 @@
-const LEVELS_LIST: string[] = ['001.json', '002.json', '003.json'];
+const LEVELS_LIST: string[] = ['001.json', '002.json', '003.json', '004.json', '005.json'];
 
 export { LEVELS_LIST };
